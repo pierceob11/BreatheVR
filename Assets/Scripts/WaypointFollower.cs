783229c83143f3aa5ae1f062b12796069d2007d6
Pierce O'Brien <pierceob11@hotmail.co.uk>
2023-05-31 00:01:38 +0200

    Improved waypoint script (Turn and locked Y-axis) & Installed URP

--- Assets/Scripts/WaypointFollower.cs
+++ Assets/Scripts/WaypointFollower.cs
@@ -6,6 +6,7 @@ public class WaypointFollower : MonoBehaviour
 {
     public Transform[] waypoints; // An array of waypoints for the object to follow
     public float movementSpeed = 5f; // The movement speed of the object
+    public float turningSpeed = 5f; // The turning speed of the object
 
     private int currentWaypointIndex = 0; // Index of the current waypoint
 
@@ -13,12 +14,17 @@ public class WaypointFollower : MonoBehaviour
     {
         // Calculate the direction to the current waypoint
         Vector3 direction = waypoints[currentWaypointIndex].position - transform.position;
+        direction.y = 0f; // Set the y-component of the direction to zero
+
+        // Smoothly rotate the object to face the waypoint
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turningSpeed * Time.deltaTime);
 
         // Normalize the direction and multiply it by the movement speed
         Vector3 movement = direction.normalized * movementSpeed * Time.deltaTime;
 
-        // Move the object towards the waypoint
-        transform.Translate(movement);
+        // Move the object towards the waypoint (excluding the y-axis)
+        transform.Translate(new Vector3(movement.x, 0f, movement.z));
 
         // Check if the object has reached the current waypoint
         if (direction.magnitude <= movement.magnitude)
