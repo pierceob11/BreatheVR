c6f26cbcce0991c0a86411ee1d43d98d458d9c12
Pierce O'Brien <pierceob11@hotmail.co.uk>
2023-06-06 15:39:43 +0200

    Landmarks now trigger Dialogue with OnTriggerEnter

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -6,6 +6,16 @@ public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
 
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            TriggerDialogue();
+            Debug.Log("Player has entered landmark trigger");
+        }
+    }
+
+
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
