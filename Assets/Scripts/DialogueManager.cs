5ba7ba9ee7bb8541556d5fdb15009f0688633d0e
Pierce O'Brien <pierceob11@hotmail.co.uk>
2023-06-06 14:51:45 +0200

    DialogueBox now animates on/off screen

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour {
 
-    private Queue<string> sentences;
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
 
     
+    private Queue<string> sentences;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +21,48 @@ public class DialogueManager : MonoBehaviour {
 
     public void StartDialogue (Dialogue dialogue)
     {
-        Debug.Log("Starting conversation with " +  dialogue.name);
+        animator.SetBool("IsOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+
     }
+
+    public void DisplayNextSentence ()
+    {
+        if (sentences.Count == 0) 
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine (TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }    
+    }
+
+    void EndDialogue ()
+    {
+        animator.SetBool("IsOpen", false);
+    }
+
 
 }
