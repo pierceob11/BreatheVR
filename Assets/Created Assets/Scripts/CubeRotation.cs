5e4748b66c2596fef1f4554afd275c9c705df57b
Pierce O'Brien <pierceob11@hotmail.co.uk>
2023-06-27 01:40:15 +0200

    Added CubeRotation script that works

--- Assets/Created Assets/Scripts/CubeRotation.cs
+++ Assets/Created Assets/Scripts/CubeRotation.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class CubeRotation : MonoBehaviour
 {
     public GameObject[] cubes;
-    public Vector3[] rotationAxes;
     public float[] rotationSpeeds;
 
     void Update()
     {
         for (int i = 0; i < cubes.Length; i++)
         {
-            cubes[i].transform.Rotate(rotationAxes[i] * rotationSpeeds[i] * Time.deltaTime);
+            Vector3 rotationAxis = Vector3.zero;
+
+            // Set the rotation axis based on the cube index
+            if (i == 0)
+                rotationAxis = Vector3.right;  // X-axis
+            else if (i == 1)
+                rotationAxis = Vector3.up;     // Y-axis
+            else if (i == 2)
+                rotationAxis = Vector3.forward; // Z-axis
+
+            cubes[i].transform.Rotate(rotationAxis * rotationSpeeds[i] * Time.deltaTime);
         }
     }
 }
